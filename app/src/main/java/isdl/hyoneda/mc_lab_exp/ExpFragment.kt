0d783f89f79f675576bcfba84f97d17b3f98ad76
h.y <string.d7@gmail.com>
2018-07-05 08:22:01 +0900

    右上メニュー削除

--- app/src/main/java/isdl/hyoneda/mc_lab_exp/ExpFragment.kt
+++ app/src/main/java/isdl/hyoneda/mc_lab_exp/ExpFragment.kt
@@ -11,6 +11,10 @@ import android.view.ViewGroup
 import android.widget.RadioGroup
 import android.widget.Toast
 
+/*
+* 環境変更画面
+*
+*/
 
 class ExpFragment : Fragment(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,25 +23,40 @@ class ExpFragment : Fragment(){
 
     // UIに関してはここをいじる
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val colorGroup = view?.findViewById<RadioGroup>(R.id.w_color_group)
-        colorGroup?.setOnCheckedChangeListener { _, checkedId: Int ->
+        val v : View = inflater.inflate(R.layout.fragment_exp, container, false)
+
+        // まとめてラジオグループをゲット
+        val waColorGroup = v.findViewById<RadioGroup>(R.id.w_color_group)
+        val waBriGroup = v.findViewById<RadioGroup>(R.id.w_bri_group)
+        val vmSituGroup = v.findViewById<RadioGroup>(R.id.vm_situ_group)
+        val vmVolumeGroup = v.findViewById<RadioGroup>(R.id.vm_volume_group)
+        
+        waColorGroup.setOnCheckedChangeListener { _, checkedId: Int ->
             when (checkedId) {
-                R.id.radio_1 -> Log.i("COLOR_BUTTON", "1")
-                R.id.radio_2 -> Log.i("COLOR_BUTTON", "2")
-                R.id.radio_3 -> Log.i("COLOR_BUTTON", "3")
-                R.id.radio_4 -> Log.i("COLOR_BUTTON", "4")
-                R.id.radio_5 -> Log.i("COLOR_BUTTON", "5")
-                R.id.radio_6 -> Log.i("COLOR_BUTTON", "6")
+                R.id.wa_color_ye -> Log.i("RADIO_OP", "1")
+                R.id.wa_color_wh -> Log.i("RADIO_OP", "2")
+                R.id.wa_color_or -> Log.i("RADIO_OP", "3")
+                R.id.wa_color_sb -> Log.i("RADIO_OP", "4")
+                R.id.wa_color_db -> Log.i("RADIO_OP", "5")
+                R.id.wa_color_gr -> Log.i("RADIO_OP", "6")
                 else -> throw IllegalArgumentException("not supported")
             }
         }
-        val briGroup = view?.findViewById<RadioGroup>(R.id.w_bri_group)
-        briGroup?.setOnCheckedChangeListener { _, checkedId: Int ->
+        waBriGroup.setOnCheckedChangeListener { _, checkedId: Int ->
             when (checkedId) {
-                R.id.w_radio_bri_high -> Log.i("COLOR_BUTTON", "11")
-                R.id.w_radio_bri_mid -> Log.i("COLOR_BUTTON", "12")
-                R.id.w_radio_bri_low -> Log.i("COLOR_BUTTON", "13")
-                R.id.w_radio_bri_zero -> Log.i("COLOR_BUTTON", "14")
+                R.id.w_radio_bri_high -> Log.i("RADIO_OP", "11")
+                R.id.w_radio_bri_mid -> Log.i("RADIO_OP", "12")
+                R.id.w_radio_bri_low -> Log.i("RADIO_OP", "13")
+                R.id.w_radio_bri_zero -> Log.i("RADIO_OP", "14")
+                else -> throw IllegalArgumentException("not supported")
+            }
+        }
+        vmVolumeGroup.setOnCheckedChangeListener { _, checkedId: Int ->
+            when (checkedId) {
+                R.id.vm_volume_0 -> Log.i("RADIO_OP", "音量0")
+                R.id.vm_volume_1 -> Log.i("RADIO_OP", "音量1")
+                R.id.vm_volume_2 -> Log.i("RADIO_OP", "音量2")
+                R.id.vm_volume_3 -> Log.i("RADIO_OP", "音量3")
                 else -> throw IllegalArgumentException("not supported")
             }
         }
--- app/src/main/java/isdl/hyoneda/mc_lab_exp/MainActivity.kt
+++ app/src/main/java/isdl/hyoneda/mc_lab_exp/MainActivity.kt
@@ -48,13 +48,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             super.onBackPressed()
         }
     }
-
+/* 右上メニューの表示
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
-
+*/
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
